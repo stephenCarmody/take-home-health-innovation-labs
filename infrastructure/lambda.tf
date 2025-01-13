4f9a8eff056f203d5e3c9d6569570ef138dd1eac
stephenCarmody <stephen.m.carmody@gmail.com>
2025-01-13 21:13:47 +0100

    feat: added s3 bucket and changed lambda arch

--- infrastructure/lambda.tf
+++ infrastructure/lambda.tf
@@ -3,6 +3,7 @@ resource "aws_lambda_function" "pii_redaction" {
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.take_home_health_innovation_labs_serving.repository_url}:serving-lambda-latest"
   role          = aws_iam_role.lambda_role.arn
+  architectures = ["arm64"]
 
   memory_size = 1024
   timeout     = 30
---
+++ infrastructure/s3.tf
@@ -0,0 +1,3 @@
+resource "aws_s3_bucket" "model_artifacts" {
+  bucket = "pii-redaction-model-artifacts"
+}
